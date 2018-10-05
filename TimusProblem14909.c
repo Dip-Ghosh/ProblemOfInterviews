aa7848365987803156247796751ce7dcbf4541f8
Dip Ghosh <39679672+Dip-Ghosh@users.noreply.github.com>
2018-10-05 23:51:50 +0600

    Timus Problem
    some online problems are solved

---
+++ TimusProblem14909.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+
+    int i,j;
+    int s=10;
+    int r;
+    scanf("%d\%d",&i,&j);
+    if(i<=10)
+        r=s-i;
+        printf("%d\n",r);
+    if(j<=10)
+        r=s-j;
+        printf("%d",r);
+        return 0;
+
+}
